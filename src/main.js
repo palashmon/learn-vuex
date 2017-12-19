697b529c129af421d34e9312c99c8ed4e51fa2de
Palash Mondal <palashm@live.in>
2017-12-19 22:10:44 +0530

    Prevent the production tip on Vue startup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Set this to false to prevent the production tip on Vue startup.
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
   render: h => h(App)
