d262317773c2bd61ab9bd6c6396d25b0d4977a9e
Palash Mondal <palashm@live.in>
2017-12-23 21:27:42 +0530

    Setting up a Central Store

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
 // Set this to false to prevent the production tip on Vue startup.
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+	el: '#app',
+	render: h => h(App)
+});
---
+++ src/store/store.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+	state: {
+		products: [
+			{ name: 'Banana Skin', price: 20 },
+			{ name: 'Shiny Star', price: 40 },
+			{ name: 'Green Shells', price: 60 },
+			{ name: 'Red Shells', price: 80 }
+		]
+	}
+});
+
+export default store;
